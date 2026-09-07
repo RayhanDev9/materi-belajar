e6c8fe27b24f209535592d83f40f55ac758112da
Muhamad Rayhan <m.rayhanoi26@gmail.com>
2026-09-07 22:05:14 +0700

    learning

--- react/studi-kasus/the-will-oasis/src/services/supabase.js
+++ react/studi-kasus/the-will-oasis/src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+export const supabaseUrl = "https://sazybtxhdagoetezxmtm.supabase.co";
+const supabaseKey = "sb_publishable_dYekqqo8l0x9Vgf8LgGC7w_K__mNeKS"; // Salin seluruh kunci dari dashboard Anda
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
